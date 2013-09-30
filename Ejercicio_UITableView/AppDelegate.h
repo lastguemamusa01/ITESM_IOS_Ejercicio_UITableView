e82b084ff155294ea80d3dfed3e0cdfea68ed003
Ma.Elena Melón Jareda <mmelon@MaElena-Melon-Jaredas-MacBook-Pro.local>
2013-09-30 11:22:14 -0500

    Initial Commit

---
+++ Ejercicio_UITableView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ejercicio_UITableView
+//
+//  Created by Ma.Elena Melón Jareda on 9/30/13.
+//  Copyright (c) 2013 Ma.Elena Melón Jareda App Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
